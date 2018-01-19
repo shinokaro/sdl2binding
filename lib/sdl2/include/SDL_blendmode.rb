4500ff7a01b251ad3af2ae9ffdf1af4947eae829
shinokaro <shinokaro@hotmail.co.jp>
2018-01-20 00:38:59 +0900

    Update
    add sdl2/include/SDL_blendmode.rb

--- lib/sdl2/include/SDL_blendmode.rb
+++ lib/sdl2/include/SDL_blendmode.rb
@@ -4,27 +4,27 @@ module SDL2
   #
 
   # Enum: SDL_BlendMode
-  SDL_BLENDMODE_NONE  = 0x00000000  # no blending
-                                    #  dstRGBA = srcRGBA
-  SDL_BLENDMODE_BLEND = 0x00000001  # alpha blending
-                                    #  dstRGB = (srcRGB * srcA) + (dstRGB * (1-srcA))
-                                    #  dstA = srcA + (dstA * (1-srcA))
-  SDL_BLENDMODE_ADD   = 0x00000002  # additive blending
-                                    #  dstRGB = (srcRGB * srcA) + dstRGB
-                                    #  dstA = dstA
-  SDL_BLENDMODE_MOD   = 0x00000004  # color modulate
-                                    #  dstRGB = srcRGB * dstRGB
-                                    #  dstA = dstA
+  SDL_BLENDMODE_NONE    = 0x00000000  # no blending
+                                      #  dstRGBA = srcRGBA
+  SDL_BLENDMODE_BLEND   = 0x00000001  # alpha blending
+                                      #  dstRGB = (srcRGB * srcA) + (dstRGB * (1-srcA))
+                                      #  dstA = srcA + (dstA * (1-srcA))
+  SDL_BLENDMODE_ADD     = 0x00000002  # additive blending
+                                      #  dstRGB = (srcRGB * srcA) + dstRGB
+                                      #  dstA = dstA
+  SDL_BLENDMODE_MOD     = 0x00000004  # color modulate
+                                      #  dstRGB = srcRGB * dstRGB
+                                      #  dstA = dstA
   SDL_BLENDMODE_INVALID = 0x7FFFFFFF
   # Additional custom blend modes can be returned by SDL_ComposeCustomBlendMode()
   typealias 'SDL_BlendMode', 'int'
 
   # Enum: SDL_BlendOperation
-  SDL_BLENDOPERATION_ADD          = 0x1  # dst + src: supported by all renderers
-  SDL_BLENDOPERATION_SUBTRACT     = 0x2  # dst - src : supported by D3D9, D3D11, OpenGL, OpenGLES
-  SDL_BLENDOPERATION_REV_SUBTRACT = 0x3  # src - dst : supported by D3D9, D3D11, OpenGL, OpenGLES
-  SDL_BLENDOPERATION_MINIMUM      = 0x4  # min(dst, src) : supported by D3D11
-  SDL_BLENDOPERATION_MAXIMUM      = 0x5  # max(dst, src) : supported by D3D11
+  SDL_BLENDOPERATION_ADD              = 0x1  # dst + src: supported by all renderers
+  SDL_BLENDOPERATION_SUBTRACT         = 0x2  # dst - src : supported by D3D9, D3D11, OpenGL, OpenGLES
+  SDL_BLENDOPERATION_REV_SUBTRACT     = 0x3  # src - dst : supported by D3D9, D3D11, OpenGL, OpenGLES
+  SDL_BLENDOPERATION_MINIMUM          = 0x4  # min(dst, src) : supported by D3D11
+  SDL_BLENDOPERATION_MAXIMUM          = 0x5  # max(dst, src) : supported by D3D11
   typealias 'SDL_BlendOperation', 'int'
 
   # Enum: SDL_BlendFactor
@@ -40,10 +40,10 @@ module SDL2
   SDL_BLENDFACTOR_ONE_MINUS_DST_ALPHA = 0xA  # 1-dstA, 1-dstA, 1-dstA, 1-dstA
   typealias 'SDL_BlendFactor', 'int'
 
-  extern 'SDL_BlendMode SDL_ComposeCustomBlendMode(SDL_BlendFactor srcColorFactor,
-                                                 SDL_BlendFactor dstColorFactor,
-                                                 SDL_BlendOperation colorOperation,
-                                                 SDL_BlendFactor srcAlphaFactor,
-                                                 SDL_BlendFactor dstAlphaFactor,
-                                                 SDL_BlendOperation alphaOperation)'
+  extern 'SDL_BlendMode SDL_ComposeCustomBlendMode(SDL_BlendFactor    srcColorFactor,
+                                                   SDL_BlendFactor    dstColorFactor,
+                                                   SDL_BlendOperation colorOperation,
+                                                   SDL_BlendFactor    srcAlphaFactor,
+                                                   SDL_BlendFactor    dstAlphaFactor,
+                                                   SDL_BlendOperation alphaOperation)'
 end
